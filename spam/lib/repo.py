964d5a31d0af7c6b7a8c8f0a272f89a8ed7c08ed
MrPetru <petrea.email@gmail.com>
2013-01-14 22:39:10 +0100

    mercurial versione 1.9 non accetta i carateri accentati questo fix dovrebbe risolvere la conversione - ma non sono sicuro

--- spam/lib/repo.py
+++ spam/lib/repo.py
@@ -72,7 +72,7 @@ def commit_single(proj, asset, filename, text, username=None):
     if isinstance(filename, list):
         raise SPAMRepoError('expected a single file for asset %s' % asset.id)
 
-    text = 'asset %s - %s' % (asset.id, text)
+    text = u'asset %s - %s' % (asset.id, text)
     encodedtext = text.encode('utf-8')
     targets = []
     
@@ -104,7 +104,7 @@ def commit_multi(proj, asset, filenames, text, username=None):
     if not isinstance(filenames, list):
         raise SPAMRepoError('expected a list of files for asset %s' % asset.id)
 
-    text = 'asset %s - %s' % (asset.id, text)
+    text = u'asset %s - %s' % (asset.id, text)
     encodedtext = text.encode('utf-8')
     target_sequence_path = asset.path.replace('#', '%04d')
     targets = []
